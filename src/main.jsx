08ad8686c1517baee2e6c3168b448cc5ee3e4960
Ashiqur <ashiqur2812@gmail.com>
2024-12-26 22:39:47 +0600

    done the initial part of the tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,23 @@ import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './router/router.jsx';
 import AuthProvider from './providers/AuthProvider.jsx';
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  </AuthProvider>
+  <QueryClientProvider>
+    <AuthProvider>
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
+    </AuthProvider>
+  </QueryClientProvider>
+
 );
